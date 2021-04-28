34a431861f801cba731bfe57f2052e333e580120
Dimitrey1978 <Dimitrey@mail.ru>
2021-04-28 16:35:45 +0300

    Тоже считал словать из файла, все получилось.

---
+++ 017-Linq-3/017-Linq-3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _017_Linq_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(@"..\..\..\dic.txt", System.Text.Encoding.Default))
+            {
+                 var dictionary = new Dictionary<dynamic, dynamic> { };
+                     
+                while (!sr.EndOfStream)
+                {
+                    string line1 = sr.ReadLine();
+                    string line2 = sr.ReadLine();
+
+                    dictionary.Add(line1, line2);
+
+                }
+                
+                foreach (var item in dictionary)
+                {
+                    Console.WriteLine($"{item.Key} - {item.Value}");
+                }
+
+            }
+
+                      
+        }
+    }
+}
